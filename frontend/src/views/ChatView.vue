fe029ad9a9a3c81242ba37eda3c6e995853dd609
HamnYce <hamadboa@gmail.com>
2024-09-04 11:49:53 -0400

    prep the skeleton

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -1,53 +1,37 @@
 <script setup lang="ts">
 const placeholder = "Empty Chat... try to type something!";
+const messages = [
+  {
+    id: 1,
+    text: "Hello, how can I help you?",
+    sender: "bot",
+  },
+  {
+    id: 2,
+    text: "I need help with my order",
+    sender: "user",
+  },
+  {
+    id: 3,
+    text: "Sure, what is your order number?",
+    sender: "bot",
+  },
+];
 </script>
 
 <template>
-  <div>
-
-    <textarea rows="4" :placeholder="placeholder" disabled
-      class="block grow form-textarea w-full resize-none rounded-lg border border-slate-300 bg-transparent p-2.5 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"></textarea>
-
-    <div
-      class="w-full rounded-xl rounded-tl-none border border-slate-300 transition-colors duration-200 focus-within:!border-primary hover:border-slate-400 dark:border-navy-450 dark:focus-within:!border-accent dark:hover:border-navy-400">
-      <label class="block">
-        <textarea rows="5" placeholder="Write a comment"
-          class="form-textarea w-full resize-none bg-transparent p-3 pb-0 placeholder:text-slate-400/70"></textarea>
-      </label>
-      <div class="flex justify-between p-2.5">
-        <div class="flex items-end space-x-1">
-          <button
-            class="btn -ml-1 size-8 rounded-full p-0 hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25">
-            <svg xmlns="http://www.w3.org/2000/svg" class="size-5" fill="none"
-              viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
-            </svg>
-          </button>
-          <button
-            class="btn size-8 rounded-full p-0 hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25">
-            <svg xmlns="http://www.w3.org/2000/svg" class="size-5" fill="none"
-              viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z" />
-            </svg>
-          </button>
-          <button
-            class="btn size-8 rounded-full p-0 hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25">
-            <svg xmlns="http://www.w3.org/2000/svg" class="size-5" fill="none"
-              viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-          </button>
-        </div>
-        <button
-          class="btn rounded-md bg-primary px-4 text-xs+ font-medium text-white hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90">
-          Comment
-        </button>
-      </div>
+  <div class="flex flex-col">
+    <div class="bg-neutral-500 grow">
+      TODO: Chat box goes here with v-for the messages
     </div>
-
-
+    <div class="flex h-20 justify-between items-center">
+      <label class="block w-full">
+        <textarea
+          class="textarea w-full items-center resize-none border-none outline-none focus:outline-none"
+          style="line-height: 1;"
+          placeholder="Try sending a message!"></textarea>
+      </label>
+      <button class="btn btn-primary rounded-full">Send</button>
+    </div>
   </div>
 </template>
