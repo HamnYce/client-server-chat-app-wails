0b3a585f3f551bde95760c7868295f5f2c666387
HamnYce <hamadboa@gmail.com>
2024-09-06 04:34:40 -0400

    add tcp client logic

--- app.go
+++ app.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"context"
-	"fmt"
+	"io"
+	"log"
+	"net"
 )
 
 // App struct
 type App struct {
-	ctx context.Context
+	ctx      context.Context
+	conn     net.Conn
+	io       bufio.ReadWriter
+	messages []string
 }
 
 // NewApp creates a new App application struct
@@ -19,9 +25,58 @@ func NewApp() *App {
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+	a.conn = nil
+	a.messages = make([]string, 0)
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+func (a *App) ConnectToChatroom() {
+	if a.conn != nil {
+		return
+	}
+
+	conn, err := net.Dial("tcp4", "localhost:9090")
+	if err != nil {
+		conn = nil
+		return
+	}
+	a.conn = conn
+	a.io = *bufio.NewReadWriter(bufio.NewReader(a.conn), bufio.NewWriter(a.conn))
+
+	log.Println("Successfully connected to server")
+}
+
+func (a *App) DisconnectFromChatroom() {
+	a.conn.Close()
+	a.conn = nil
+}
+
+func (a *App) SendMsgToChatRoom(msg string) {
+	a.io.WriteString(msg)
+	a.io.Flush()
+}
+
+func (a *App) ListenForMessage() {
+	if !a.IsConnected() {
+		return
+	}
+	a.io.Reader.Reset(a.conn)
+	str, err := a.io.ReadString(0)
+
+	// _, err := a.conn.Read(a.buffer)
+	if err == io.EOF {
+		a.DisconnectFromChatroom()
+	} else if err != nil {
+		log.Println(err)
+		a.DisconnectFromChatroom()
+	}
+	log.Println("Received From Server:", str)
+	a.messages = append(a.messages, str)
+}
+
+func (a *App) GetMessages() []string {
+	return a.messages
+}
+
+func (a *App) IsConnected() bool {
+	return a.conn != nil
 }
