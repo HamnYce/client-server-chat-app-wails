83e71e90ee9172141513a48d07a63ba04cdf2272
HamnYce <hamadboa@gmail.com>
2024-09-06 04:36:45 -0400

    hook up go bindings to connect to server

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -1,37 +1,60 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { ConnectToChatroom, GetMessages, IsConnected, ListenForMessage, SendMsgToChatRoom } from '../../wailsjs/go/main/App';
+
+let isConnected = ref(false);
 const placeholder = "Empty Chat... try to type something!";
-const messages = [
-  {
-    id: 1,
-    text: "Hello, how can I help you?",
-    sender: "bot",
-  },
-  {
-    id: 2,
-    text: "I need help with my order",
-    sender: "user",
-  },
-  {
-    id: 3,
-    text: "Sure, what is your order number?",
-    sender: "bot",
-  },
-];
+let messages = ref<string[]>([]);
+let userMessage = ref("");
+
+const _interval = setInterval(async () => {
+  isConnected.value = await IsConnected();
+  if (isConnected.value) {
+    await ListenForMessage();
+    console.log(messages);
+    messages.value = await GetMessages();
+  }
+}, 1000);
+
 </script>
 
 <template>
   <div class="flex flex-col">
-    <div class="bg-neutral-500 grow">
-      TODO: Chat box goes here with v-for the messages
+    <div class="flex flex-col grow">
+      <div v-if="isConnected" class="grow">
+        {{ messages }}
+        <!-- <div v-for="message, i in messages" :key="i">
+        <div v-if="message.sender === 'bot'" class="flex justify-start">
+          <div class="bg-gray-200 p-2 m-2 rounded-lg chat-bubble">
+            {{ message.text }}
+          </div>
+        </div>
+        <div v-else class="flex justify-end">
+          <div class="bg-blue-500 text-white p-2 m-2 rounded-lg">
+            {{ message.text }}
+          </div>
+        </div>
+      </div> -->
+      </div>
+      <div v-else class="grow flex justify-center items-center">
+        <span>Please
+          <button class="btn btn-primary" @click="ConnectToChatroom"> Connect
+          </button>
+        </span>
+      </div>
+      <hr class="w-11/12 self-center">
+      <div class="flex h-20 justify-between items-center px-10">
+        <label class="block w-3/4">
+          <textarea
+            class="textarea w-full items-center resize-none border-none outline-none focus:outline-none"
+            style="line-height: 1;" placeholder="Try sending a message!"
+            v-model="userMessage" />
+
+        </label>
+        <button class="btn btn-primary grow max-w-40 text-lg"
+          @click="SendMsgToChatRoom(userMessage)">Send</button>
+      </div>
     </div>
-    <div class="flex h-20 justify-between items-center">
-      <label class="block w-full">
-        <textarea
-          class="textarea w-full items-center resize-none border-none outline-none focus:outline-none"
-          style="line-height: 1;"
-          placeholder="Try sending a message!"></textarea>
-      </label>
-      <button class="btn btn-primary rounded-full">Send</button>
-    </div>
+
   </div>
 </template>
